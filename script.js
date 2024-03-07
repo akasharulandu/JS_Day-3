f6ceaa2aff2a0897a4fd9887426ccc669904436f
akasharulandu <jemiyonakash37@gmail.com>
2024-03-07 12:33:54 +0530

    Day3

---
+++ script.js
@@ -0,0 +1,59 @@
+//Introduction to array:-
+// var a =[1,2,5,"abc",true]
+// console.log(a);
+// for(var i=0;i<a.length;i=i+1){
+//     console.log(a[i]);
+// }
+
+// console.log(a.length);
+// console.log(a.length-1);
+// console.log(a.indexOf(2.5));
+// a[100]=50;
+// console.log(a.length);
+// console.log(a[99])
+
+//Introduction to Objects:-
+//Personel details(Name,age,gender,year,city)
+
+var personDetails = {
+    Name: "Akash",
+    Age: 23,
+    Gender: "Male",
+    Year: 2000,
+    City: "Chennai  "
+}
+    // personDetails.Emailid = "akasha@abc.in" //add
+    // personDetails.phone=9786109228 //add
+    // personDetails.Age = 24 //update
+    // delete personDetails.Year //delete
+    // personDetails.Education = ["10th", "Diploma","B.Tech"]
+    // console.log(personDetails);
+    // for(var i=0;i<personDetails.Education.length;i=i+1){
+    //   console.log(personDetails.Education[i])
+    // }
+
+    //Introduction to JSON
+    // var obj ={
+    //   "name":"AS",
+    //   "age":"24",
+    //   "gender":"female"
+    // }
+
+    // console.log(obj.name);
+    // console.log(obj.age);
+    // console.log(obj.gender);
+
+    //Array Of Objects
+// var arr =[
+// {
+//   "name":"Joe",
+//   "Age": "24"
+// },
+// {
+//   "name":"Jemi  ",
+//   "Age": "24"
+// }
+// ]
+// for (var i=0;i<arr.length;i++){
+//   console.log(arr[i].name,arr[i].Age) 
+// }
